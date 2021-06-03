eda5ddcfb133af99d5799afea5c783272d76ddf9
Bohdan2241 <bohdanyezhlov@gmail.com>
2021-06-03 17:40:40 +0300

    add getName

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const getName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + getName + '!');
+
+export default getName;
