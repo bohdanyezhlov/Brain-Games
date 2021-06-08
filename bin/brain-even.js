8a71b54b6336d4f920318a8bd2312272cd23b033
Bohdan2241 <bohdanyezhlov@gmail.com>
2021-06-08 03:40:33 +0300

    add brain-calc

--- bin/brain-even.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import brainEven from '../src/even.js';
-
-brainEven();
---
+++ src/index.js
@@ -0,0 +1,93 @@
+import readlineSync from 'readline-sync';
+import { welcome, name } from './cli.js';
+
+// brain-even.js
+
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+const getRandomNum = () => {
+  const randomNum = Math.round(Math.random() * 100);
+  return randomNum;
+};
+
+const brainEven = () => {
+  welcome();
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let step = 0; step <= 3;) {
+    if (step === 3) {
+      console.log(`Congratulations, ${name}!`);
+      break;
+    }
+
+    const number = getRandomNum();
+    console.log(`Qusetion: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (isEven(number) && (answer === 'yes')) {
+      console.log('Correct!');
+      step += 1;
+    } else if (!(isEven(number)) && (answer === 'no')) {
+      console.log('Correct!');
+      step += 1;
+    } else if (!(isEven(number)) && (answer !== 'no')) {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${name}!`);
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${name}!`);
+    }
+  }
+};
+
+// brain-calc.js
+
+const getOperation = () => {
+  const operations = ['+', '-', '*'];
+  const randomNum = Math.floor(Math.random() * 3);
+  const operation = operations[randomNum];
+  return operation;
+};
+
+const brainCalc = () => {
+  welcome();
+
+  console.log('What is the result of the expression?');
+
+  for (let step = 0; step <= 3;) {
+    if (step === 3) {
+      console.log(`Congratulations, ${name}!`);
+      break;
+    }
+
+    const firstNumber = getRandomNum();
+    const secondNumber = getRandomNum();
+    const operation = getOperation();
+    console.log(`Qusetion: ${firstNumber} ${operation} ${secondNumber}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    let solution = 0;
+
+    if (operation === '+') {
+      solution = firstNumber + secondNumber;
+    } else if (operation === '-') {
+      solution = firstNumber - secondNumber;
+    } else {
+      solution = firstNumber * secondNumber;
+    }
+
+    if (Number(answer) === solution) {
+      console.log('Correct!');
+      step += 1;
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${solution}'. \nLet's try again, ${name}!`);
+    }
+  }
+};
+
+export { brainEven, brainCalc };
